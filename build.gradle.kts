b1fda2900cad9df845a3b7cb6844482d662f1aa7
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-10-21 17:02:40 +0200

    Add Lucene dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+dependencies {
+    val luceneVersion = "10.3.1"
+    implementation("org.apache.lucene:lucene-core:$luceneVersion")
+    implementation("org.apache.lucene:lucene-analysis-common:${luceneVersion}")
+}
+
 
 tasks.test {
     useJUnitPlatform()
